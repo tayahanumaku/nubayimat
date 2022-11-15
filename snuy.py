6a2e85d35c15f9ac2c8af8a09fd9e4bfb9f23027
Your Name <you@example.com>
2022-11-15 14:09:37 -0500

    auto

--- snuy.py
+++ snuy.py
@@ -1,5 +1,5 @@
 import  sys,os
 if __name__=='__main__':
- ll=os.popen('echo "ynaumoi"').read()
- z=3*str(ll)
- print(z+str(2.92))
+ ll=os.popen('echo "iwanchi"').read()
+ z=5*str(ll)
+ print(z+str(812.1))
