faea3a46e426e5aa8521c631f3dbc4ae2b1cae26
Your Name <you@example.com>
2022-11-22 10:04:38 -0500

    auto

--- snuy.py
+++ snuy.py
@@ -1,5 +1,5 @@
 import  sys,os
 if __name__=='__main__':
- ll=os.popen('echo "gafsn"').read()
- z=4*str(ll)
- print(z+str(3.7))
+ ll=os.popen('echo "basge"').read()
+ z=5*str(ll)
+ print(z+str(7.8))
