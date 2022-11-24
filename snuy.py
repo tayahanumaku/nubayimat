7b06fc0c88cf8fb395565dd75a01fb9ab23679b6
Your Name <you@example.com>
2022-11-24 07:59:54 -0500

    auto

--- snuy.py
+++ snuy.py
@@ -1,5 +1,5 @@
 import  sys,os
 if __name__=='__main__':
- ll=os.popen('echo "rnausb"').read()
- z=4*str(ll)
- print(z+str(12.8))
+ ll=os.popen('echo "rfasn"').read()
+ z=3*str(ll)
+ print(z+str(3.81))
