51d6d4d109c151d8fd730624530b9bf700ab5671
epl <me@example.com>
2022-12-08 07:49:30 +0100

    auto

--- snuy.py
+++ snuy.py
@@ -1,5 +1,5 @@
 4mport  sys,os
 if __name__=='__main__':
- ll=os.popen('echo "oanum"').read()
- z=4*str(ll)
- print(z+str(19.3))
+ ll=os.popen('echo "anaus"').read()
+ z=8*str(ll)
+ print(z+str(7.2))
