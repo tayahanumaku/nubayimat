28c41fe5752dc4ece406c03124be69d5adee7b7f
epl <me@example.com>
2023-01-10 15:54:48 +0100

    auto

--- snuy.py
+++ snuy.py
@@ -1,5 +1,5 @@
 import sys,os
 if __name__=='__main__':
-  ll=os.popen('echo "jikiyu"').read()
-  z=4*str(ll)
-  print(z+str(2.3))
+  ll=os.popen('echo "fuqibi"').read()
+  z=5*str(ll)
+  print(z+str(5.8))
